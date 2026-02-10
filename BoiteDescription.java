643c4bd713d15b71aadffa9f7ee7f8969d9ab345
kadlex0 <alexandre.damman@etu.univ-littoral.fr>
2026-02-10 13:24:26 +0100

    Refactor import statements to use the new Couleur constructor across multiple classes

--- BoiteDescription.java
+++ BoiteDescription.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import MG2D.geometrie.Texture;	
-import MG2D.geometrie.Couleur;
+import MG2D.Couleur;
 import MG2D.geometrie.Point;
 import MG2D.geometrie.Rectangle;
 import MG2D.geometrie.Texte;
@@ -70,23 +70,23 @@ public class BoiteDescription extends Boite{
 	}
 	
 	//declaration des textes bouton + joystick
-	this.tJoystick = new Texte(Couleur .NOIR, "...", font1, new Point(760, 80));
+	this.tJoystick = new Texte(new Couleur(0, 0, 0), "...", font1, new Point(760, 80));
 	for(int i = 0 ; i < 3 ; i++){
-	    this.tBouton[i] = new Texte(Couleur .NOIR, "...", font1, new Point(910+130*i, 120));
+	    this.tBouton[i] = new Texte(new Couleur(0, 0, 0), "...", font1, new Point(910+130*i, 120));
 	}
 	for(int i = 3 ; i < 6 ; i++){
-	    this.tBouton[i] = new Texte(Couleur .NOIR, "...", font1, new Point(910+130*(i-3), 40));
+	    this.tBouton[i] = new Texte(new Couleur(0, 0, 0), "...", font1, new Point(910+130*(i-3), 40));
 	}
 	stop = false;
 	message = new Texte[10];
 	for(int i = 0 ; i < message.length ; i++){
-	    message[i] = new Texte(Couleur .NOIR, "", font2, new Point(960, 590));
+	    message[i] = new Texte(new Couleur(0, 0, 0), "", font2, new Point(960, 590));
 	    message[i].translater(0, -i*30);
 
 	}
 	nombreLigne = 0;
 
-	highscore = new Texte(Couleur.NOIR, "HIGHSCORE", font3, new Point(960, 335));
+	highscore = new Texte(new Couleur(0, 0, 0), "HIGHSCORE", font3, new Point(960, 335));
 	listeHighScore = new Texte[10];
 	for(int i=0;i<5;i++){
 	    listeHighScore[i] = new Texte(new Couleur(0, 0, 0), "", font4, new Point(820,310));
