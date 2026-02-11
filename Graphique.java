f46b475e93992b21a6ba7bb7d2e81bead099bb17
kadlex0 <alexandre.damman@etu.univ-littoral.fr>
2026-02-11 14:46:52 +0100

    Improve music handling by adding null checks and preventing playback when no music is available

--- Graphique.java
+++ Graphique.java
@@ -269,12 +269,17 @@ public class Graphique {
     }
     
     public static void lectureMusiqueFond() {
+	if(cptMus <= 0 || tableauMusiques == null || tableauMusiques.length == 0){
+	    return;
+	}
     	musiqueFond = new Bruitage ("sound/bg/"+tableauMusiques[(int)(Math.random()*cptMus)]);
     	musiqueFond.lecture();
     }
 	
 	public static void stopMusiqueFond(){
-		musiqueFond.arret();
+		if(musiqueFond != null){
+			musiqueFond.arret();
+		}
 	}
 	
 	public static void afficherTexte(int valeur){
